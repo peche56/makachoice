dd47d2a0c80257ca571c1dbcc8cb12ad5757fbb2
peche56 <c.filleul56@gmail.com>
2017-04-08 20:17:06 +0200

    html and ctrl ok

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,4 +1,26 @@
 angular.module('app')
     .controller('MainController', function($scope) {
-      /* Here is your main controller */
+
+$scope.goodreasons=[];
+
+
+$scope.good= function() {
+  var reason = {
+  texte : $scope.reason,
+  val : $scope.value
+};
+ $scope.goodreasons.push(reason);
+$scope.reason='';
+};
+
+$scope.evilreasons=[];
+
+$scope.evil= function() {
+  var reason = {
+    texte : $scope.reason,
+    val : $scope.value
+  };
+  $scope.evilreasons.push(reason);
+  $scope.reason='';
+};
     });
